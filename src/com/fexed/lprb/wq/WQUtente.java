c533992edc103bb5ea342d82df4de7502260e590
Federico Matteoni <fedex15@live.it>
2020-01-24 14:22:38 +0100

    javadoc

--- src/com/fexed/lprb/wq/WQUtente.java
+++ src/com/fexed/lprb/wq/WQUtente.java
@@ -2,12 +2,35 @@ package com.fexed.lprb.wq;
 
 import java.util.ArrayList;
 
+/**
+ * @author Federico Matteoni
+ */
 public class WQUtente {
+    /**
+     * Il nome utente
+     */
     public String username;
+
+    /**
+     * La password
+     */
     public String password;
+
+    /**
+     * Il punteggio
+     */
     public int points;
+
+    /**
+     * La lista amici
+     */
     public ArrayList<String> friends;
 
+    /**
+     * Inizializza un nuovo utente
+     * @param username Il nickname
+     * @param password La password
+     */
     public WQUtente(String username, String password) {
         this.username = password;
         this.password = password;
@@ -15,6 +38,13 @@ public class WQUtente {
         this.friends = new ArrayList<>();
     }
 
+    /**
+     * Inserisce un utente esistente
+     * @param username Il nickname
+     * @param password La password
+     * @param points Il punteggio
+     * @param friends La lista amici
+     */
     public WQUtente(String username, String password, int points, ArrayList<String> friends) {
         this.username = password;
         this.password = password;
