e59374c5f8f56632931cf3fc0bf52f9ac6bfcf1c
Federico Matteoni <fedex15@live.it>
2020-01-28 13:55:57 +0100

    Comments

--- src/com/fexed/lprb/wq/server/WQHandler.java
+++ src/com/fexed/lprb/wq/server/WQHandler.java
@@ -16,16 +16,57 @@ import java.util.HashMap;
  * @author Federico Matteoni
  */
 public class WQHandler implements Runnable {
+
+    /**
+     * Riferimento al server
+     */
     private WQServer server;
+
+    /**
+     * Lista delle parole da tradurre per la sfida, null finché non inizia la sfida
+     */
     public HashMap<String, String> randomWords;
+
+    /**
+     * Indica se l'utente è nel corso di una sfida
+     */
     public boolean isChallenging = false;
+
+    /**
+     * Punteggio ottenuto durante la singola sfida
+     */
     public int pointsMade;
+
+    /**
+     * Canale di comunicazione
+     */
     private SocketChannel skt;
+
+    /**
+     * Il nickname dell'utente che viene gestito da questo handler
+     */
     public String username;
+
+    /**
+     * Indica se l'handler deve lavorare o disconnettersi
+     */
     private boolean online;
+
+    /**
+     * Chiave per la comunicazione
+     */
     private SelectionKey key;
+
+    /**
+     * Porta UDP su cui inoltrare le richieste di sfida
+     */
     public int challengePort;
 
+    /**
+     * Costruttore che prepara gli attributi principali
+     * @param server Riferimento al server
+     * @param skt Socket di comunicazione
+     */
     public WQHandler(WQServer server, SocketChannel skt) {
         this.server = server;
         this.skt = skt;
@@ -37,7 +78,6 @@ public class WQHandler implements Runnable {
      * @param str Il testo da spedire
      */
     public void send(String str) {
-        System.out.println(str + " to " + username);
         try {
             ByteBuffer buff = ByteBuffer.wrap(str.getBytes(StandardCharsets.UTF_8));
             int n;
@@ -47,16 +87,20 @@ public class WQHandler implements Runnable {
         } catch(Exception ignored) {}
     }
 
+    /**
+     * Avvia la sfida con il singolo client. Questo metodo si preoccupa di inviare le parole da tradurre, attendere
+     * la traduzione ricevuta e calcolare il punteggio ottenuto dal singolo client.
+     */
     public void startChallenge() {
-        isChallenging = true;
-        this.pointsMade = 0;
+        isChallenging = true; //Segnala che l'utente è nel mezzo di una sfida
+        this.pointsMade = 0; //Inizializza il punteggio
         ByteBuffer bBuff = ByteBuffer.allocate(128);
-        String str;
         int n;
         try {
             for (String word : randomWords.keySet()) {
-                send("challengeRound:" + word);
+                send("challengeRound:" + word); //Manda la parola
 
+                //Attende e legge la risposta
                 do {
                     try { Thread.sleep(100); }
                     catch (InterruptedException ignored) {}
@@ -68,32 +112,33 @@ public class WQHandler implements Runnable {
                 } while (n > 0);
                 bBuff.flip();
                 String received = StandardCharsets.UTF_8.decode(bBuff).toString();
-                String command = received.split(":")[0];
-                String translatedWord = received.split(":")[1];
+                String command = received.split(":")[0]; //Per verifica che il client risponda correttamente
+                String translatedWord = received.split(":")[1]; //Ricevo "challengeAnswer:<qualcosa>"
                 String wordToGet = randomWords.get(word).toLowerCase().replaceAll("!", "");
                 WQServerController.gui.updateStatsText("(" + this.username + ", " + word + "): " + translatedWord + " - " + wordToGet);
 
                 if (command.equals("challengeAnswer")) {
-                    if (translatedWord.equals("-1")) pointsMade += 0;
-                    else if (translatedWord.toLowerCase().contains(wordToGet)) pointsMade += 2;
-                    else pointsMade -= 1;
+                    if (translatedWord.equals("-1")) pointsMade += 0; //Timeout, non ho risposto, 0 punti
+                    else if (translatedWord.toLowerCase().contains(wordToGet)) pointsMade += 2; //Ho risposto bene, +X
+                    else pointsMade -= 1; //Ho risposto sbagliando, -Y punti
                 }
             }
         } catch (IOException ex) { send("ERR: " + ex.getMessage()); }
         send("challengeRound:-3");
 
-        isChallenging = false;
+        isChallenging = false; //Segnala che l'utente non è più all'interno della sfida
         randomWords = null;
     }
 
     /**
      * Inoltra via UPD la richiesta di sfida proveniente da {@code nickSfidante}
      * @param nickSfidante Il nickname dell'utente che ha richiesto la sfida
-     * @return il socket se la richiesta viene accettata, null altrimenti
+     * @return il socket se la richiesta viene accettata, {@code null} altrimenti
      */
     public DatagramSocket challenge(String nickSfidante, int port) {
         WQServerController.gui.updateStatsText("(" + this.username + "): " + nickSfidante + " vuole sfidarmi!");
         try {
+            //Invio della richiesta di sfida "challengeRequest:<nickSfidante>"
             DatagramSocket datagramSocket = new DatagramSocket();
             datagramSocket.connect(InetAddress.getByName("127.0.0.1"), this.challengePort);
             datagramSocket.setSoTimeout(10000); //10s per accettare la sfida
@@ -101,11 +146,13 @@ public class WQHandler implements Runnable {
             DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
             datagramSocket.send(datagramPacket);
 
+            //Attesa della risposta o dello scadere del timeout
             try {
                 buffer = new byte[128];
                 datagramPacket = new DatagramPacket(buffer, buffer.length);
                 datagramSocket.receive(datagramPacket);
                 String received = StandardCharsets.UTF_8.decode(ByteBuffer.wrap(datagramPacket.getData())).toString();
+                //Ricevo "challengeResponse:OK" oppure "challengeResponse:NO"
                 String response = received.split(":")[1].trim();
                 if ("OK".equals(response)) {
                     WQServerController.gui.updateStatsText(this.username + " ha accettato!");
@@ -123,6 +170,7 @@ public class WQHandler implements Runnable {
 
     @Override
     public void run() {
+        //Inizializzazione dati di comunicazione
         ByteBuffer bBuff = ByteBuffer.allocate(128);
         String str;
 
@@ -131,29 +179,32 @@ public class WQHandler implements Runnable {
             Selector selector = Selector.open();
             key = skt.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
             do {
-                if (randomWords == null) {
+                if (randomWords == null) { //Se non ho una sfida in attesa
                     int n;
                     do {
                         Thread.sleep(100);
                         bBuff.clear();
                         n = ((SocketChannel) key.channel()).read(bBuff);
                     } while (n == 0 && randomWords == null);
-                    if (n == -1) online = false;
+                    if (n == -1) online = false; //Disconnessione
                     else if (randomWords == null) {
+                        // /Ricontrollo perché durante l'attesa+lettura potrei aver ricevuto una richiesta di sfida
                         do {
                             n = ((SocketChannel) key.channel()).read(bBuff);
                         } while (n > 0);
                         bBuff.flip();
+
+                        //Parsing della stringa ricevuta
                         String received = StandardCharsets.UTF_8.decode(bBuff).toString();
                         String command = received.split(":")[0];
                         switch (command) {
-                            case "login":
+                            case "login": //"login:<nomeutente> <password>"
                                 if (this.username == null) {
                                     String name = received.split(":")[1].split(" ")[0];
                                     String pwd = received.split(":")[1].split(" ")[1];
                                     System.out.println("Verifica " + name + " " + pwd);
                                     n = this.server.login(name, pwd, this);
-                                    if (n == 0) {
+                                    if (n == 0) { //Login a buon fine
                                         this.username = name;
                                         str = "answer:OK";
                                         ByteBuffer buff = ByteBuffer.wrap(str.getBytes(StandardCharsets.UTF_8));
@@ -180,9 +231,9 @@ public class WQHandler implements Runnable {
                                             n = ((SocketChannel) key.channel()).read(buff);
                                         } while (n > 0);
                                         buff.flip();
-                                        received = StandardCharsets.UTF_8.decode(buff).toString(); //challengePort;
+                                        received = StandardCharsets.UTF_8.decode(buff).toString(); //challengePort
                                         challengePort = Integer.parseInt(received.split(":")[1]);
-                                    } else if (n == -1) {
+                                    } else if (n == -1) { //Segnalazione degli errori
                                         str = "answer:ERR1";
                                         ByteBuffer buff = ByteBuffer.wrap(str.getBytes(StandardCharsets.UTF_8));
                                         do {
@@ -211,7 +262,7 @@ public class WQHandler implements Runnable {
                                         } while (n > 0);
                                         online = false;
                                     }
-                                } else {
+                                } else { //Questo handler sta già gestendo un utente
                                     str = "answer:ERR";
                                     ByteBuffer buff = ByteBuffer.wrap(str.getBytes(StandardCharsets.UTF_8));
                                     do {
@@ -219,7 +270,7 @@ public class WQHandler implements Runnable {
                                     } while (n > 0);
                                 }
                                 break;
-                            case "showonlinelist": {
+                            case "showonlinelist": { //Richiesta della lista amici
                                 String json = this.server.mostraOnline();
                                 str = "onlinelist:".concat(json);
                                 ByteBuffer buff = ByteBuffer.wrap(str.getBytes(StandardCharsets.UTF_8));
@@ -228,17 +279,17 @@ public class WQHandler implements Runnable {
                                 } while (n > 0);
                                 break;
                             }
-                            case "addfriend": {
+                            case "addfriend": { //Aggiunta di un amico
                                 try {
                                     String name = received.split(":")[1];
                                     n = this.server.aggiungiAmico(this.username, name);
-                                    if (n == 0) { //amicizia creata
+                                    if (n == 0) { //L'amicizia viene creata con successo
                                         str = "answer:OKFREN";
                                         ByteBuffer buff = ByteBuffer.wrap(str.getBytes(StandardCharsets.UTF_8));
                                         do {
                                             n = ((SocketChannel) key.channel()).write(buff);
                                         } while (n > 0);
-                                    } else {
+                                    } else { //Segnalazione degli errori
                                         str = "answer:ERR ";
                                         if (n == -1) str = str.concat(name + " non esistente.");
                                         else if (n == -2) str = str.concat("Sei già amico con " + name);
@@ -259,7 +310,7 @@ public class WQHandler implements Runnable {
                                 }
                                 break;
                             }
-                            case "showfriendlist": {
+                            case "showfriendlist": { //Richiesta della lista amici
                                 String json = this.server.listaAmici(this.username);
                                 str = "friendlist:".concat(json);
                                 ByteBuffer buff = ByteBuffer.wrap(str.getBytes(StandardCharsets.UTF_8));
@@ -268,7 +319,7 @@ public class WQHandler implements Runnable {
                                 } while (n > 0);
                                 break;
                             }
-                            case "showpoints": {
+                            case "showpoints": { //Richiesta del punteggio utente
                                 int points = this.server.mostraPunteggio(this.username);
                                 str = "userpoints:".concat(points + "");
                                 ByteBuffer buff = ByteBuffer.wrap(str.getBytes(StandardCharsets.UTF_8));
@@ -277,7 +328,7 @@ public class WQHandler implements Runnable {
                                 } while (n > 0);
                                 break;
                             }
-                            case "showranking": {
+                            case "showranking": { //Richiesta della classifica
                                 String json = this.server.mostraClassifica(this.username);
                                 str = "ranking:".concat(json);
                                 ByteBuffer buff = ByteBuffer.wrap(str.getBytes(StandardCharsets.UTF_8));
@@ -286,12 +337,12 @@ public class WQHandler implements Runnable {
                                 } while (n > 0);
                                 break;
                             }
-                            case "challenge": {
+                            case "challenge": { //Richiesta di sfida
                                 String name = received.split(":")[1];
                                 this.server.sfida(this.username, name);
                                 break;
                             }
-                            default:
+                            default: //Messaggio di testo generico
                                 WQServerController.gui.updateStatsText("(" + username + "): " + received);
                                 break;
                         }
