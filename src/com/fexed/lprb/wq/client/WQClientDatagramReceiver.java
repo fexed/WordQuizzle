f6d7c36b3536297f824f3b0f934bdbcfa4839c4c
Federico Matteoni <fedex15@live.it>
2020-01-28 13:59:50 +0100

    Comments

--- src/com/fexed/lprb/wq/client/WQClientDatagramReceiver.java
+++ src/com/fexed/lprb/wq/client/WQClientDatagramReceiver.java
@@ -12,7 +12,15 @@ import java.nio.charset.StandardCharsets;
  * @author Federico Matteoni
  */
 public class WQClientDatagramReceiver implements Runnable {
+
+    /**
+     * Il socket su cui ascoltare i pacchetti UDP
+     */
     private DatagramSocket datagramSocket;
+
+    /**
+     * Segnala se sta già gestendo una sfida o no
+     */
     public static boolean isChallenging = false;
 
     public WQClientDatagramReceiver(DatagramSocket datagramSocket) {
@@ -26,20 +34,22 @@ public class WQClientDatagramReceiver implements Runnable {
 
         do {
             try {
+                //Ricezione del datagramma
                 buff = new byte[128];
                 datagramPacket = new DatagramPacket(buff, buff.length);
                 datagramSocket.receive(datagramPacket);
                 String received = StandardCharsets.UTF_8.decode(ByteBuffer.wrap(buff)).toString();
                 String command = received.split(":")[0];
-                if (command.equals("challengeRequest")) {
-                    if (isChallenging) {
+                if (command.equals("challengeRequest")) { //Se è richiesta di sfida "challengeRequest:<nickname>"
+                    if (isChallenging) { //Se sta già sfidando rifiuta automaticamente
                         buff = "challengeResponse:NO".getBytes(StandardCharsets.UTF_8);
                         datagramPacket = new DatagramPacket(buff, buff.length, datagramPacket.getAddress(), datagramPacket.getPort());
                         datagramSocket.send(datagramPacket);
-                    } else {
+                    } else { //Altrimenti chiede all'utente
                         WQClientController.gui.updateCommText("Sfida ricevuta...");
                         int n = WQClientController.gui.showChallengeDialog(received.split(":")[1]);
                         buff = new byte[128];
+                        //In base alla scelta dell'utente accetta o rifiuta
                         if (n == JOptionPane.OK_OPTION) {
                             WQClientController.gui.updateCommText("Sfida accettata!");
                             buff = "challengeResponse:OK".getBytes(StandardCharsets.UTF_8);
