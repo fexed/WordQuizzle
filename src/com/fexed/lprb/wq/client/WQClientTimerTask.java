1f8e20759fe07c733b7ca6c4db18eabb0be6fec6
Federico Matteoni <fedex15@live.it>
2020-01-28 14:02:39 +0100

    Comments

--- src/com/fexed/lprb/wq/client/WQClientTimerTask.java
+++ src/com/fexed/lprb/wq/client/WQClientTimerTask.java
@@ -3,9 +3,14 @@ package com.fexed.lprb.wq.client;
 import java.util.TimerTask;
 
 /**
+ * TimerTask per il timeout di risposta durante la sfida. Invia "-1" al server
  * @author Federico Matteoni
  */
 public class WQClientTimerTask extends TimerTask {
+
+    /**
+     * Riferimento al client
+     */
     private WQClient client;
 
     public WQClientTimerTask(WQClient client) {
